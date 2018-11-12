5c7853e52de46b29aa21a869c2830cee116ef70f
Dima Tkachenko <dt.mail.gma@gmail.com>
2018-11-12 13:57:12 +0100

    Converter Celsius to Fahrenheit

--- convert.cpp
+++ convert.cpp
@@ -6,6 +6,18 @@ using namespace std;
 
 int main(int ca, char* pszArgs[])
 {
-cout << "hello world";
+int celsius;
+cout << "Input Temp. in Celsius:";
+cin >> celsius;
+int factor;
+factor = 212 - 32;
+int fahrenheit;
+fahrenheit = factor * celsius/100 + 32;
+cout << "Temp. by fahrenheit:";
+cout << fahrenheit;
+cout << endl;
 return 0;
 }
+
+
+
